e6a9ce06fa261a18ed3b123abc43d4871216f44e
areif-dev <ajreifsnyder@protonmail.com>
2024-02-21 17:43:30 -0500

    Add report 1-1

--- src/reports.rs
+++ src/reports.rs
@@ -1,5 +1,39 @@
 use crate::{wait, UIElement, SHORT_WAIT_MS};
 
+/// Control ABC Client4 to generate a 11 report (INVENTORY LISTING)
+///
+/// # Arguments
+///
+/// * `abc_window` - The `UIElement` representing the Client4 window
+/// * `starting_sku` - The first bill to send to send to the 11
+/// * `ending_sku` - The last invoice to send to the 11 report
+///
+/// # Returns
+///
+/// Will return unit type if successful. Return `uiautomation::Error` if UI manipulation fails at
+/// any point
+///
+/// # Errors
+///
+/// Will return `Err(uiautomation::Error)` if UI manipulation fails at any point
+pub fn generate_report_11(
+    abc_window: &UIElement,
+    starting_sku: u64,
+    ending_sku: u64,
+) -> uiautomation::Result<()> {
+    abc_window.send_keys("{F10}1", SHORT_WAIT_MS * 3)?;
+    wait(SHORT_WAIT_MS * 5);
+    abc_window.send_keys("1{enter}", SHORT_WAIT_MS / 2)?;
+    wait(SHORT_WAIT_MS * 5);
+    abc_window.send_keys("I", SHORT_WAIT_MS / 2)?;
+    wait(SHORT_WAIT_MS * 5);
+    abc_window.send_keys(
+        &format!("{{enter}}{}{{enter}}{}{{enter}}t", starting_sku, ending_sku),
+        SHORT_WAIT_MS / 2,
+    )?;
+    Ok(())
+}
+
 /// Control ABC Client4 to generate a 214 report (BILL DETAIL)
 ///
 /// # Arguments
